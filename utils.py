cdc7cb67cab7d83d7c87f9ea17ea47614a8cdcf2
Kwesi Dadson <samson.dadson@turntabl.io>
2024-08-28 14:43:13 +0000

    done

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def save_config(data: dict, file: str):
     with open(file, "w") as f:
         json.dump(data, f)
 
-
+#print err
 def print_err(err):
     output = str(err) + " on line " + str(sys.exc_info()[2].tb_lineno)
     Logger.fatal(output)
