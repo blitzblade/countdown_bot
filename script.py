d19e2ed6664ef269bca38a37ca26c8879ea96a5d
Kwesi Dadson <samson.dadson@turntabl.io>
2024-09-11 13:26:50 +0000

    6

--- script.py
+++ script.py
@@ -9,7 +9,6 @@ config = load_config()
 consumer_key = config['consumer_key']
 consumer_secret = config['consumer_secret']
 access_token = config['access_token']
-
 access_secret = config['access_secret']
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
